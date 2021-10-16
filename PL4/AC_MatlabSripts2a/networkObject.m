502918f96757b19602d2d1f7febc9ff23dd91958
Francisco Fernandes <52376925+DrSh1ny@users.noreply.github.com>
2021-10-16 15:18:08 +0100

    network

---
+++ PL4/AC_MatlabSripts2a/networkObject.m
@@ -0,0 +1,26 @@
+load P.mat P
+net = network(1,1,[1],[1],[0],[1]);
+net.layers{1}.transferFcn = 'purelin';
+
+rangeInp=[0 1];
+rangeInp=repmat(rangeInp,256,1);
+net.inputs{1}.size=256;
+net.inputs{1}.range=rangeInp;
+
+
+net.trainFcn = 'traingda';
+net.performParam.lr = 0.5; % learning rate
+net.trainParam.epochs = 1000; % maximum epochs
+net.trainParam.show = 40; % show
+net.trainParam.goal = 1e-6; % goal=objective
+net.performFcn = 'sse'; % criterion
+
+nAmostras=600;
+t = eye(10);
+t=repmat(t,1,60);
+
+net=train(net,P(:,1:nAmostras),t(:,1:nAmostras));
+
+q=sim(net,P);
+
+perform(net,t,q)
