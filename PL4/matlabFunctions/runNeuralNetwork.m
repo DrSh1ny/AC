e4ca40c49c8ab10cd1a99c41c2154fbf726e6826
JoaoMarcelino <j.13.marcelino@gmail.com>
2021-11-03 13:51:17 +0000

    runNeuralNetwork updates

--- PL4/matlabFunctions/runNeuralNetwork.m
+++ PL4/matlabFunctions/runNeuralNetwork.m
@@ -1,11 +1,21 @@
 function runNeuralNetwork(filter, layerNumber,activationFunction)
 %RUNNEURALNETWORK Summary of this function goes here
 %   Detailed explanation goes here
-    filters=['None','Associative Memory','Binary Perceptron'];
-    layerNumbers=['1','2'];
-    activationFunctions=['Linear','Hard Limit','Sigmoidal'];
+    filters=['None' 'Associative Memory' 'Binary Perceptron'];
+    layerNumbers=['1' '2'];
+    activationFunctions=['Linear' 'Hard Limit' 'Sigmoidal'];
     
-    load 'net_1_sig.mat' net;
+    
+    net = append('net_', layerNumber)
+    net = append(net, '_')
+    net = append(net, activationFunction)
+    net = append(net, '.mat')
+  
+    
+    %#function network 
+    load(net, 'net');
+    
+    net = network(net);
     load 'P.mat' P;
     load 'ind.mat' ind;
     
