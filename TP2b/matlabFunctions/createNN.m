27d8e4aa2a71e61f0f5892e42bbc488b38e6a7c5
JoaoMarcelino <j.13.marcelino@gmail.com>
2021-11-15 21:48:04 +0000

    autoencoder

--- TP2b/matlabFunctions/createNN.m
+++ TP2b/matlabFunctions/createNN.m
@@ -23,3 +23,21 @@ X = sprintf(' Accuracy: %.4f \n Sensibility %.4f \n Specificity %.4f',accuracy, 
 disp(X);
 
 
+% %AutoEncoder
+% 
+% [trainX,trainY,testX,testY]=loadDataset("../dataset/44202.mat",0.8);
+% trainY(find(trainY==3))=1;
+% testY(find(testY==3))=1;
+% targets=zeros(2,size(trainY,2));
+% for i=1:size(trainY, 2)
+%     targets(trainY(i),i)=1;
+% end
+% 
+% autoenc = trainAutoencoder(trainX);
+% XReconstructed = predict(net,testX);
+% mseError = mse(testX-XReconstructed)
+% 
+% 
+% [accuracy, sensibility, specificity] = Measures(autoenc, testX, XReconstructed);
+% X = sprintf(' Accuracy: %.4f \n Sensibility %.4f \n Specificity %.4f',accuracy, sensibility, specificity);
+% disp(X);
